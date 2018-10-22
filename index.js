cb5b24959b49eb3711ecf536fe1488f0ea4baa86
Romuald Brillout <github.com@brillout.com>
2018-10-22 19:24:40 +0200

    show nicer error message when running in IE

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const fetch = (
-    typeof window !== "undefined" ? (
-        window.fetch
-    ) : (
-        // we use `eval('require')` instead of `require` to
-        // make sure that webpack doesn't bundle `node-fetch`
-        eval('require')('node-fetch')
-    )
-);
+"use strict";
+
+var assert_usage = require('reassert/usage');
+
+var fetch;
+
+if( isUsingBrowserBuiltIn() ) {
+  assert_usage(
+    false && envSupportsFetch(),
+    [
+      "This browser doesn't seem to support `fetch` (window.fetch===undefined).",
+      "Is this running in Internet Explorer?",
+      "Note that this fetch library doesn't support IE and if you want to support IE then use another fetch implementation instead."
+    ].join('\n')
+  );
+  fetch = window.fetch;
+} else {
+  // we use `eval('require')` instead of `require` to
+  // make sure that webpack doesn't bundle `node-fetch`
+  fetch = eval('require')('node-fetch');
+}
 
 module.exports = fetch;
+module.exports.isUsingBrowserBuiltIn = isUsingBrowserBuiltIn;
+
+function isUsingBrowserBuiltIn() {
+  return typeof window !== "undefined";
+}
+
+function envSupportsFetch() {
+  return window.fetch!==undefined;
+}
